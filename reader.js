dbcd377b22a62790f2a830d907aee8eb928872b1
Massimiliano Cattaneo <mcattaneo@theworkshop.com>
2017-09-18 15:53:38 +0200

    add callto batch file

--- reader.js
+++ reader.js
@@ -2,10 +2,8 @@ const pdfreader = require('pdfreader');
 const fs = require('fs');
 const path = require('path');
 var json2csv = require('json2csv');
-var NodeXls = require('node-xls');
-var order = ['file', 'id', 'contributes'];
-var fieldMap = {file: 'NOME DEL FILE', id:'NUMERO DI SERIE', contributes: 'CONTRIBUTI'};
-var tool = new NodeXls();
+var fields = ['file', 'id', 'contributes'];
+var fieldNames = ['NOME DEL FILE', 'NUMERO DI SERIE', 'CONTRIBUTI'];
 
 function read(fileName, regEx, discounts) {
 	const files = [];
@@ -24,8 +22,8 @@ function read(fileName, regEx, discounts) {
 							contributes: contributes.filter((f, i) => contributes.indexOf(f) === i).join(',')
 						});
 						if (index === (dir.length - 1)) {
-							var result = tool.json2xls(files, { order, fieldMap });
-							fs.writeFileSync(path.resolve('./output/' + fileName), result, 'binary');
+							var result = json2csv({ data: files, fields: fields, del: ';', fieldNames });
+							fs.writeFileSync(path.resolve('./output/' + fileName), result);
 							console.log('READED: ', files.length, 'PDF FILES');
 							res(files);
 						}
